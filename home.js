b556d68ce08a18c887c334cb8d523dab4a325331
Pratap  Shirame <115365865+PratapShirame@users.noreply.github.com>
2024-05-30 12:14:41 +0000

    Calculate the tip using a ternary operator

--- home.js
+++ home.js
@@ -14,3 +14,14 @@ switch(day) {
         console.log("Pratap Shirame");
 
 } 
+
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+
+const tip = (bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.20);
+const finalPrice = `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`;
+console.log(finalPrice);
+
+
