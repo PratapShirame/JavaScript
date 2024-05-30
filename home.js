8873bf52576c1ff56ca76cd1bcabfab53bcfd2aa
Pratap  Shirame <115365865+PratapShirame@users.noreply.github.com>
2024-05-30 07:21:19 +0000

    Swich Statemets

--- home.js
+++ home.js
@@ -1,1 +1,16 @@
-console.log("hellow")
+//console.log("hellow")
+
+const day = "";
+switch(day) {
+    case 'Monday':
+        console.log("Pratap");
+        break;
+
+    case 'Tuseday':
+        console.log("Shirame");
+        break;
+
+    default:
+        console.log("Pratap Shirame");
+
+} 
