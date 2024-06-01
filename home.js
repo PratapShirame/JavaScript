ff0f9423cf4e3b539519b72084edff101a6d0f1f
Pratap  Shirame <115365865+PratapShirame@users.noreply.github.com>
2024-06-01 19:17:33 +0000

    Write a fuction

--- home.js
+++ home.js
@@ -1,27 +1,43 @@
 //console.log("hellow")
 
-const day = "";
-switch(day) {
-    case 'Monday':
-        console.log("Pratap");
-        break;
+// `const day = "";
+// switch(day) {
+//     case 'Monday':
+//         console.log("Pratap");
+//         break;
 
-    case 'Tuseday':
-        console.log("Shirame");
-        break;
+//     case 'Tuseday':
+//         console.log("Shirame");
+//         break;
 
-    default:
-        console.log("Pratap Shirame");
+//     default:
+//         console.log("Pratap Shirame");
 
-} 
+// } `
 
-const bill = 275;
-
+// const bill = 275;
 /* Write your code below. Good luck! 🙂 */
+// const tip = (bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.20);
+// const finalPrice = `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`;
+// console.log(finalPrice);
 
 
-const tip = (bill >= 50 && bill <= 300) ? (bill * 0.15) : (bill * 0.20);
-const finalPrice = `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`;
-console.log(finalPrice);
+
+// Write a fuction 
+function logger(){
+    console.log("Pratap Shirame ");
+}
+logger();
+
+function fruitProcessor ( apples, oranges){
+    const juice = `juice with ${apples} apples amd ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor( 30, 5);
+console.log(orangeJuice);
 
 
