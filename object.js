83a1259e5331dbfd23db463869fbdf9b0e3b2af9
Pratap  Shirame <115365865+PratapShirame@users.noreply.github.com>
2024-06-02 13:27:14 +0000

    Calculate the age

--- object.js
+++ object.js
@@ -18,3 +18,32 @@ getSummary: function (){
 }
 console.log(`${Pratap.firstName} is a ${Pratap.calAge()} year old ${Pratap.job} and ${Pratap.getSummary()}.`);
 
+const john ={
+    fullName : 'John Smith',
+    mass:78,
+    height:1.69,
+
+    calcBMI: function(){
+        this.bmi=this.mass/(this.height*this.height);
+        return this.bmi;
+    }
+}
+const mark ={
+    fullName : 'Mark Miller',
+    mass:92,
+    height:1.95,
+
+    calcBMI: function(){
+        this.bmi=this.mass/(this.height*this.height);
+        return this.bmi;
+    }
+}
+//"John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+const higherBMI = function(){
+    if (john.calcBMI >= mark.calcBMI){
+     return 'higher';
+    }else{
+        return 'less';
+    }
+}
+console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is ${higherBMI()} than ${mark.fullName}'s (${mark.calcBMI()})`);
