7cc2058759483a87c9737ccdab32caac4443ef32
Pratap  Shirame <115365865+PratapShirame@users.noreply.github.com>
2024-06-04 16:52:47 +0000

    Array

---
+++ array.js
@@ -0,0 +1,19 @@
+// array 
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros =["Ajay", " Sunil", "Aksshay"]
+console.log(myArr);
+
+// revers array 
+myArr.reverse();
+console.log(myArr);
+
+// Combine tow diffrece array in the single array
+const combinArr= myArr.concat(myHeros);
+console.log(combinArr);
+
+// Add the new element in the aaray 
+const newArr = myArr.push("Pratap");
+console.log(newArr);
+
+console.log(combinArr);
